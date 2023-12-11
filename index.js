7b6b0e08ba31b1a4842cc093f03b3655ebaec51d
emirhncann <emirhncann01@gmail.com>
2023-12-11 16:44:10 +0300

    kantin

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require("express");
-var http = require("http");
+const http = require("http");
 const cors = require("cors");
+
 const app = express();
-const port = process.env.PORT ||3000;
-var server = http.createServer(app);
-var io = require("socket.io")(server,{
-    cors:
-    {
-        origin:"*",
-    }
+const port = process.env.PORT || 7162;
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
 });
-//middlewre
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-io.on("connection", (socket) => {
+try {
+  io.on("connection", (socket) => {
     console.log("connected");
     console.log(socket.id, "has joined");
-    socket.on("/test", (msg) => {
-        console.log(msg);
+
+    // /test endpoint'ine gelen POST isteğini dinleyen route
+    app.post("/test", (req, res) => {
+      const receivedData = req.body;
+      console.log("Received data from /test endpoint:", receivedData);
+
+      // Burada gelen veriyi işleyebilir ve gerekirse soketleri güncelleyebilirsiniz
+      io.emit("testEvent", receivedData); // Tüm soketlere gelen veriyi ilet
+      res.status(200).json({ success: true, message: "Data received successfully" });
     });
-});
+  });
+} catch (error) {
+  console.log(error);
+}
 
 server.listen(port, () => {
-    console.log("server started on port", port);
+  console.log("Server started on port", port);
 });
